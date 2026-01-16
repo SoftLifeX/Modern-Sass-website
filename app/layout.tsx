0c27ba47e2f43f739b105378a410cc1fba962201
Daniel Daniel c <softlife.dev001@gmail.com>
2026-01-16 10:15:25 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 export const metadata: Metadata = {
 	title: "Light Sass Landing Page",
-	description: "Light Sass Landing Page by devwithzain",
+	description: "Light Sass Landing Page",
 };
 export default function RootLayout({
 	children,
